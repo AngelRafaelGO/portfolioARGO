8438b1dec946fb97dec35c8664eef0a718e10384
Angel Rafael <generalpompeyo@hotmail.com>
2022-06-06 13:07:20 +0200

    Added firebase to project Added CRUD operations : read & write for like counter

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -2,19 +2,63 @@ import React, { useState } from "react";
 import { Box,IconButton, Heading, Text, Image, useColorModeValue } from "@chakra-ui/react";
 import { VscArrowRight } from "react-icons/vsc";
 import { BsHeart } from "react-icons/bs";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore, doc, getDoc, updateDoc } from "firebase/firestore/lite";
 
 import Section from '../../components/modal/motionDiv'
 import amyIdle from "../../assets/amy_idle.gif"
 import Chris_me from '../../assets/chris_me.svg';
 import "./header.css";
 
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDXBue5Si_s5EGPrwMh1t3MXMvYuqmKndQ",
+    authDomain: "mywebsite-10750.firebaseapp.com",
+    projectId: "mywebsite-10750",
+    storageBucket: "mywebsite-10750.appspot.com",
+    messagingSenderId: "796603426673",
+    appId: "1:796603426673:web:13121669eb019ae39cb8d0",
+    measurementId: "G-BY4LW6BD1Q"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const firestore = getFirestore(app);
+
+let amyLikes = 0;
+// Amy read likes
+async function readCounter() {
+    const myLikes = await getDoc(counter);
+    if (myLikes.exists()) {
+        const docData = myLikes.data();
+        amyLikes = docData.likes;
+    }
+}
+
+// Amy add likes
+const counter = doc(firestore, 'counter/amyLikes');
+function writeLikeCounter() {
+    amyLikes += 1;
+    const docData = {
+        likes: amyLikes,
+    };
+
+    updateDoc(counter, docData);
+}
+
 function Header() {
 
-    let counter = 0;
-    const [likeCount, setLikeCount] = useState(counter);
-    function setCounter() {
-        setLikeCount(likeCount + 1);
-    }
+    // let counter = 0;
+    // const [likeCount, setLikeCount] = useState(counter);
+    // function setCounter() {
+    //    setLikeCount(likeCount + 1);
+    // }
 
     return (
         <Box className="header__main_div">
@@ -53,11 +97,11 @@ function Header() {
                                         className="header__like_button"
                                         icon={<BsHeart />}
                                         bg={useColorModeValue('gray.200', 'gray.600')}
-                                        onClick={setCounter}
+                                        onClick={writeLikeCounter}
                             />
                             <Text
                                 color={useColorModeValue('red.600', 'pink.600')}
-                                className="header__like_counter">{likeCount}</Text>
+                                className="header__like_counter">{amyLikes}</Text>
                         </Box>
                     </Box>
                     <Box className="header__amy_container_image_interaction">
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { BrowserRouter} from "react-router-dom";
+
 import App from './App';
 import './index.css';
 

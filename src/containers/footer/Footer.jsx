2fd9c0e41c659c649221ebc9292f1e472246d600
Angel Rafael <generalpompeyo@hotmail.com>
2022-10-01 17:00:37 +0200

    footer color fix

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -5,20 +5,22 @@ import { AiOutlineCopyright } from "react-icons/ai";
 import "./footer.css";
 
 function Footer() {
-    return (
-        <Box className="main__footer_div"
-             bg={useColorModeValue('#008F8C', '#F2A516')}
-             color={useColorModeValue('white', 'black')}>
-            <Box className="footer__copyright">
-                <Text>Angel Rafael Gonzalez</Text>
-                <Text>All Rights Reserved</Text>
-                <Box className="footer__copyright_logo">
-                    <AiOutlineCopyright/>
-                    <Text>2022</Text>
-                </Box>
-            </Box>
+  return (
+    <Box
+      className="main__footer_div"
+      bg={useColorModeValue("#B8E6DC", "#F2A516")}
+      color={useColorModeValue("black", "black")}
+    >
+      <Box className="footer__copyright">
+        <Text>Angel Rafael Gonzalez</Text>
+        <Text>All Rights Reserved</Text>
+        <Box className="footer__copyright_logo">
+          <AiOutlineCopyright />
+          <Text>2022</Text>
         </Box>
-    );
+      </Box>
+    </Box>
+  );
 }
 
 export default Footer;
